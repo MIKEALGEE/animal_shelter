c9c88f97f8d11e054a429ac0612949867b2b67c7
Michael Griffin <michaelgriffin121@gmail.com>
2018-10-11 13:31:41 +0100

    starting css

--- db/animal_shelter.sql
+++ db/animal_shelter.sql
@@ -8,7 +8,7 @@ CREATE TABLE customers (
   id SERIAL4 PRIMARY KEY,
   first_name VARCHAR(255),
   last_name VARCHAR(255),
-  appointment DATE,
+  appointment VARCHAR(255),
   requirements VARCHAR(255)
 );
 
